587ae431f37b3d8a1e00ea9467de6c182c9e50e4
Dave Tech <olisenyem@gmail.com>
2023-03-17 02:54:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function that prints 
+ *
+ * Return:  always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multiligual puzzel"):
+	return (0):
+}
