b52b71bc9e24e79e25cfaac7d51a328b0eac7ded
Dave_Tech <olisenyem@gmail.com>
2024-01-09 05:26:34 +0100

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -6,9 +6,9 @@ const firstArg = process.argv[2];
 const x = parseInt(firstArg);
 
 if (!isNaN(x)) {
-  for (let i = 0; i < x; i++) {
+    for (let i = 0; i < x; i++) {
     console.log("C is fun");
-  }
+    }
 } else {
-  console.log("Missing number of occurrences");
+    console.log("Missing number of occurrences");
 }
